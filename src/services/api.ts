ddc336abd9837362769e5d9410ad36409a9446e3
Kevin Simahi <151976678+KevinMtz123@users.noreply.github.com>
2025-04-29 02:19:29 -0600

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://skyboundapi.somee.com/api",
+  baseURL: "https://skyboundapiprueba.somee.com/api",
 });
 
 api.interceptors.request.use(config => {
